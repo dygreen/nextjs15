bb43c9d453c994202d3e508d31a56b04007e60cd
dygreen <ehdus9877@naver.com>
2025-02-21 21:44:56 +0900

    #feat : sanity - cache and live API

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import SearchForm from '@/components/SearchForm'
 import StartupCard, { StartupTypeCard } from '@/components/StartupCard'
 import { STARTUPS_QUERY } from '@/sanity/lib/queries'
-import { client } from '@/sanity/lib/client'
+import { sanityFetch, SanityLive } from '@/sanity/lib/live'
 
 export default async function Home({
     searchParams,
@@ -9,7 +9,7 @@ export default async function Home({
     searchParams?: Promise<{ query?: string }>
 }) {
     const query = (await searchParams)?.query
-    const posts = await client.fetch(STARTUPS_QUERY)
+    const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY })
 
     return (
         <>
@@ -37,6 +37,8 @@ export default async function Home({
                     )}
                 </ul>
             </section>
+
+            <SanityLive />
         </>
     )
 }
